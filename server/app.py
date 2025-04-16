520f553788349ef28e70ba78e0dd50ff42c925d4
louis-ngugi <louisngugi858@gmail.com>
2025-04-16 09:51:02 +0300

    update everything

--- server/app.py
+++ server/app.py
@@ -17,11 +17,38 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    @app.route('/plants', methods=['GET'])
+    def get_plants():
+        plants = Plant.query.all()
+        return jsonify([plant.to_dict() for plant in plants])
+    
+    @app.route('/plants', methods=['POST'])
+    def create_plant():
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return jsonify(new_plant.to_dict()), 201
+
+
+    
 
 class PlantByID(Resource):
-    pass
-        
+    @app.route('/plants/<int:id>', methods=['GET'])
+    def get_plant(id):
+        plant = Plant.query.get(id)
+        if plant:
+            return jsonify(plant.to_dict())
+        return {"error": "Plant not found"}, 404
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
